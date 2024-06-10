e7239a06468e6a71e1cb671ed5419f18691c7b05
Siddharth Malhotra <52989183+siddharth195@users.noreply.github.com>
2024-06-11 01:38:54 +0530

    frontend

---
+++ CreateTodo.jsx
@@ -0,0 +1,8 @@
+export function CreateTodo() {
+    return <div>
+        <input type = "text" placeholder="title"></input><br></br> <br></br>
+        <input type = "text" placeholder="Description"></input> <br></br> <br></br>
+        <button> Add a todo</button>
+        
+    </div>
+}
